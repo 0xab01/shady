c4781dafa9146b6c9b15f1a27478d667a80a9262
Nom544ad <97664456+Nom544ad@users.noreply.github.com>
2023-12-15 10:34:00 -0800

    LogIn Signup

--- shad/src/App.tsx
+++ shad/src/App.tsx
@@ -1,9 +1,10 @@
-import DarkModeChatArea from '@/pages/Chat';
+import DarkModeChatArea from '@/pages/table';
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import Route from react-router-dom
 import LoginAccount from '@/pages/Login';
 import Mainpage from '@/pages/home';
 import { ModeToggle } from "./components/ui/mode-toggle";
+import Signup  from '@/pages/signup';
 
 function App() {
   return (
@@ -13,8 +14,10 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LoginAccount />} />
-            <Route path="/noob" element={<Mainpage />} />
+            <Route path="/noob" element={<Mainpage onTabSelect={undefined} />} />
             <Route path="/chat" element={<DarkModeChatArea />} />
+            <Route path="/signup" element={<Signup />} />
+            
            
           </Routes>
         </BrowserRouter>
--- shad/src/pages/signup.tsx
+++ shad/src/pages/signup.tsx
@@ -1,34 +1,28 @@
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+// Signup.tsx
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-export default function LoginAccount() {
-    const navigate = useNavigate();
-    const handleLogin = () => {
-      // Here you would typically add your login logic
-      // For now, let's just navigate to the MainPage on button click
-      navigate('/chat');
-    };
+export default function Signup() {
+  const navigate = useNavigate();
+
+  const handleSignup = () => {
+    // Add your signup logic here
+    // For now, let's just navigate to the home page
+    navigate('/login');
+  };
+
   return (
     <div className="relative flex flex-col justify-center items-center min-h-screen overflow-hidden">
       <div className="w-full m-auto lg:max-w-lg">
         <Card>
           <CardHeader className="space-y-1">
-            <CardTitle className="text-2xl text-center">Sign in</CardTitle>
-            <CardDescription className="text-center">
-              Enter your email and password to login
-            </CardDescription>
+            <CardTitle className="text-2xl text-center">Sign up</CardTitle>
           </CardHeader>
           <CardContent className="grid gap-4">
             <div className="grid gap-2">
@@ -45,23 +39,24 @@ export default function LoginAccount() {
                 htmlFor="terms"
                 className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
               >
-                Remember me
+                I agree to the terms and conditions
               </label>
             </div>
           </CardContent>
           <CardFooter className="flex flex-col">
-          <Button className="w-full" onClick={handleLogin}>Login</Button>
+            <Button className="w-full" onClick={handleSignup}>
+              Sign up
+            </Button>
             <p className="mt-2 text-xs text-center text-gray-700">
-              {" "}
-              Don't have an account?{" "}
-              <Link to="/signup" className="text-blue-600 hover:underline">
-              <span className=" text-blue-600 hover:underline">Sign up</span>
+              Already have an account?{' '}
+              {/* Link to navigate back to the login page */}
+              <Link to="/Login" className="text-blue-600 hover:underline">
+              <span className=" text-blue-600 hover:underline">Log In</span>
               </Link>
-              
             </p>
           </CardFooter>
         </Card>
       </div>
     </div>
-  )
+  );
 }
